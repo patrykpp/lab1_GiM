da6400a23253dba3bb22072e4d2e4345d86287e9
patrykpp <patryk583@vp.pl>
2018-03-09 21:44:26 +0100

    lab1_GiM
    lab1_GiM

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+    private Rigidbody rd;
+	public float szybkosc;
+	public GUIText countText = new GUIText();
+    public GUIText winText = new GUIText();
+
+  
+	private int licznik;
+    public float speed;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        winText.text = "";
+        countText.text = "";
+        rb = GetComponent<Rigidbody>();
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+    void OnTriggerEnter(Collider other) 
+	{
+		if(other.gameObject.name == "PickUp")
+		{
+			other.gameObject.SetActive(false);
+			licznik = licznik + 1;
+			SetCountText();
+
+        }
+
+        if (other.gameObject.name == "PickUp_3")
+        {
+            other.gameObject.SetActive(false);
+            licznik = licznik + 3;
+            SetCountText();
+
+        }
+    }
+	
+	void SetCountText ()
+	{
+		countText.text = "Liczba punktów: " + licznik.ToString();
+		if(licznik >= 9)
+		{            
+			winText.text = "Wygrałeś !";
+            Time.timeScale = 0;
+        }
+    }
+}
